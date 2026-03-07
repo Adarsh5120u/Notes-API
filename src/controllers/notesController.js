8c622f7d184b53a86faf280babe50b29d041f952
Adarsh5120u <ac6256545@gmail.com>
2026-03-07 23:42:58 +0530

    Implemented Delete Operation And Completed all the CRUD operations

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -3,6 +3,7 @@ let notes =[];
 function getNotes(req,res){
     res.json(notes);
 }
+
 function getNoteById(req,res){
     const id = parseInt(req.params.id);
     const note= notes.find(n=>n.id===id);
@@ -17,16 +18,30 @@ function createNotes(req,res){
         content:content
     };
     notes.push(newNote);
-    res.json(newNote);
+    res.json({message : "Note created successfully", 
+              note: newNote});
 }
+
 function updateNote(req,res){
     const id=parseInt(req.params.id);
     const note = notes.find(n=>n.id===id);
     const {title,content} = req.body;
     if(!note){return res.status(404).json("Note not found");}
-    
+
     if(title){note.title=title;}
     if(content){note.content=content;}
     res.json(note);
 }
-module.exports={getNotes,createNotes,getNoteById,updateNote};
+
+function deleteNote(req,res){
+    const id =parseInt(req.params.id);
+    const index = notes.findIndex(n=>n.id===id);
+    if(index==-1){
+        return res.status(404).json("Note not found");
+    }
+    deletedNote=notes.splice(index,1);
+    res.json({message : "Note Deleted Successfully ",
+              deletedNote: deletedNote[0]});
+}
+
+module.exports={getNotes,createNotes,getNoteById,updateNote,deleteNote};
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -6,5 +6,6 @@ router.get('/',notesController.getNotes);
 router.post('/',notesController.createNotes);
 router.get('/:id',notesController.getNoteById);
 router.put('/:id',notesController.updateNote);
+router.delete('/:id',notesController.deleteNote);
 
 module.exports=router;
